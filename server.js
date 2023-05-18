e8a198a08153dd7ac991310db430b01e2af9b47a
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-05-18 16:54:09 +0100

    adds source folder

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.APP_PORT;
-const ticketRouter = require('./routes/index.routes');
+const ticketRouter = require("./src/routes/index.routes");
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the ticket server');
+app.get("/", (req, res) => {
+  res.send("Welcome to the ticket server");
 });
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
   })
 );
 
@@ -21,7 +21,7 @@ ticketRouter(app);
 
 app.listen(port, (err) => {
   if (err) {
-    console.error('Something bad happened');
+    console.error("Something bad happened");
   } else {
     console.log(`Server is listening on ${port}`);
   }
--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const TicketController = require ("../controllers/ticket.controller.js")
+const TicketController = require("../controllers/ticket.controller.js");
 const ticketRouter = express.Router();
 
 // Route to get all tickets
-ticketRouter.get("/", TicketController.getAllTickets)
+ticketRouter.get("/", TicketController.getAllTickets);
 
 // // Route to change status
 // ticketRouter.put("/:id", TicketController.updateTicket)
 
 module.exports = ticketRouter;
-
